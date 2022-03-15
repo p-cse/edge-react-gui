bccda55da8281a0a220732cbb2378b08c2d8b91d
Pavel Korshunov <pavelk@techjedi.co>
2022-03-15 12:22:12 +0300

    remove old env

--- src/util/corePlugins.js
+++ src/util/corePlugins.js
@@ -97,8 +97,7 @@ export const swapPlugins = {
   // shapeshift: ENV.SHAPESHIFT_INIT,
   sideshift: ENV.SIDESHIFT_INIT,
   switchain: ENV.SWITCHAIN_INIT,
-  transfer: true,
-  totle: ENV.TOTLE_INIT
+  transfer: true
 }
 
 export const allPlugins = {
